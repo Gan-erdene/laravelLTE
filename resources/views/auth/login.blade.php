2bfb7fe7418fdeb431d1ee4eabe20d50af2cc0b6
Gan-Erdene <ganaa0830@gmail.com>
2016-10-22 19:54:51 -0700

    Login huudas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,29 @@
-@extends('layouts.app')
 
-@section('content')
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Админ Нэвтрэх Хуудас</title>
 
-<div class="login-logo">
-  <a href="#"><b>Админ хуудас</a>
-</div>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
+  <link rel="stylesheet" href="../admin/bootstrap/css/bootstrap.min.css">
+
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+
+  <link rel="stylesheet" href="../admin/dist/css/AdminLTE.min.css">
+
+  <link rel="stylesheet" href="../admin/plugins/iCheck/square/blue.css">
+
+
+</head>
+<body class="hold-transition login-page">
+
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../admin/index2.html"><b>Админ хуудас</a>
+  </div>
 <div class="login-box-body">
   <p class="login-box-msg">Нэвттэрч орох</p>
 
@@ -48,9 +66,24 @@
       Google+</a>
   </div>
 
-
   <a href="{{ url('/password/reset') }}">Нууц үг сэргээх</a><br>
-  <a href=""{{ url('/register') }}" class="text-center">Шинээр бүртгүүлэх</a>
+  <a href="{{ url('/register') }}" class="text-center">Шинээр бүртгүүлэх</a>
 
 </div>
-@endsection
+</div>
+<script src="../admin/plugins/jQuery/jquery-2.2.3.min.js"></script>
+
+<script src="../admin/bootstrap/js/bootstrap.min.js"></script>
+
+<script src="../admin/plugins/iCheck/icheck.min.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%'
+    });
+  });
+</script>
+</body>
+</html>
