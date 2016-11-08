5ce79ec6062901015b9139706d744920fdd1d917
javkhlantur <javkhlantur.g@gmail.com>
2016-11-08 21:45:52 +0800

    signup checked validation

---
+++ resources/lang/mn/strings.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'signup' => 'Бүртгүүлэх',
+    'signup_user'=>'Шинээр хэрэглэгч болох',
+    'first_name'=>'Нэр',
+    'last_name'=>'Овог',
+    'email_address'=>'Имэйл хаяг',
+    'password'=>'Нууц үг',
+    'repassword'=>'Дахин нууц үг',
+    'require_first_name' =>'Нэрээ оруулна уу',
+    'require_last_name' =>'Эцэг/Эхийн нэрээ оруулна уу',
+    'require_email' =>'Имэйл хаягаа оруулна уу',
+    'require_password' =>'Нууц үгээ оруулна уу',
+    'format_email' =>'Имэйлийн формат буруу байна',
+    'valid_repassword'=>'Дахин нууц үг буруу байна'
+];
--- resources/views/frontend/signup.blade.php
+++ resources/views/frontend/signup.blade.php
@@ -1,22 +1,85 @@
 
 <div class="card-block center">
   <h4 class="m-b-0">
-    <span class="icon-text">Sign Up</span>
+    <span class="icon-text">{{ trans('strings.signup') }}</span>
   </h4>
-  <p class="text-muted">Create a new account</p>
-  <form action="index.html" method="get">
+  <p class="text-muted">{{ trans('strings.signup_user') }}</p>
+  <form id="signupForm" action="/frontend/signup" method="post" role="form">
     <div class="form-group">
-      <input type="text" class="form-control" placeholder="Full Name">
+      <input type="text" name="firstname" class="form-control" placeholder="{{trans('strings.first_name')}}">
     </div>
     <div class="form-group">
-      <input type="email" class="form-control" placeholder="Email">
+      <input type="text" name="lastname" class="form-control" placeholder="{{trans('strings.last_name')}}">
     </div>
     <div class="form-group">
-      <input type="password" class="form-control" placeholder="Password">
+      <input type="email" name="email" class="form-control" placeholder="{{trans('strings.email_address')}}">
     </div>
     <div class="form-group">
-      <input type="password" class="form-control" placeholder="Confirm Password">
+      <input type="password" name="password" class="form-control" placeholder="{{trans('strings.password')}}">
     </div>
-    <button type="submit" class="btn btn-azure">Register</button>
+    <div class="form-group">
+      <input type="password" id="repassword" name="repassword" class="form-control" placeholder="{{trans('strings.repassword')}}">
+    </div>
+    <button type="submit" class="btn btn-azure">{{ trans('strings.signup') }}</button>
   </form>
 </div>
+<script src="/frontend/assets/js/jquery.1.11.0.validate.min.js"></script>
+<script>
+$(document).ready(function () {
+  jQuery.validator.addMethod("equals", function(value, element) {
+    return this.optional(element) || value === $('#repassword').val();
+  }, "{{trans('strings.valid_repassword')}}");
+
+  $('#signupForm').validate({ // initialize the plugin
+      rules: {
+          firstname: {
+              required: true,
+          },
+          lastname:{
+            required: true
+          },
+          email:{
+            required: true, email: true,
+          },
+          password:{
+            required: true,equals:true
+          }
+      },
+      messages:{
+        firstname: {
+            required: "{{trans('strings.require_first_name')}}",
+        },
+        lastname: {
+            required: "{{trans('strings.require_last_name')}}",
+        },
+        email:{
+          required: "{{trans('strings.require_email')}}",
+          email: "{{trans('strings.format_email')}}",
+        },
+        password:{
+          required: "{{trans('strings.require_password')}}",
+        }
+      },
+      highlight: function(element, error) {
+
+        // add a class "has_error" to the element
+        $(element).closest('.form-group').addClass('has-error');
+      },
+      unhighlight: function(element) {
+          $(element).parent().removeClass('has-error');
+      },
+    errorPlacement: function (error, element) {
+      $(error).closest('label').addClass('control-label');
+      if (element.parent('.input-group').length || element.prop('type') === 'checkbox' || element.prop('type') === 'radio') {
+          error.insertAfter(element.parent());
+      } else {
+          error.insertAfter(element);
+      }
+    },
+      submitHandler: function (form) { // for demo
+            form.submitHandler();
+        }
+  });
+
+});
+</script>
