66becd2874fecd9428f07b473d8ef7880a7cd5d3
javkhlanturg <javkhlantur.g@gmail.com>
2016-11-14 21:00:09 +0800

    newsfeed controller nemew

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -43,7 +43,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="{{route('frontendHome')}}"><b>DayDay</b></a>
+          <a class="navbar-brand" href="{{route('newsfeedIndex')}}"><b>DayDay</b></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
