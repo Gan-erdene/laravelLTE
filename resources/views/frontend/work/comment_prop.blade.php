f0067ff0d38d500ceb910b3555e9815e0554b09a
javkhlanturg <javkhlantur.g@gmail.com>
2016-12-05 22:12:04 +0800

    register fixed

--- resources/views/frontend/work/comment_prop.blade.php
+++ resources/views/frontend/work/comment_prop.blade.php
@@ -3,3 +3,11 @@
     @include('frontend.work.comments', ['comments'=>$p->comment()])
   </div>
 </div>
+<div class="box-footer">
+    <div class="input-group">
+        <input type="text" data-id="{{$p->id}}" id="comm_{{$p->id}}" class="form-control comment" placeholder="Сэтгэгдэл...">
+        <span class="input-group-btn">
+            <button class="btn btn-default" data-id="{{$p->id}}" id="btnCommentSend" type="button">Илгээх</button>
+        </span>
+    </div>
+</div>
