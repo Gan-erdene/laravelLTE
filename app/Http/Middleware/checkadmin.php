f6fb507db4fb9300fe749d098f2e2588aa9a2568
javkhlanturg <javkhlantur.g@gmail.com>
2017-02-04 13:27:19 +0800

    zuwhun admin humuus admin backend url uruu handana

--- app/Http/Middleware/checkadmin.php
+++ app/Http/Middleware/checkadmin.php
@@ -3,9 +3,10 @@
 namespace App\Http\Middleware;
 
 use Closure;
-
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 class checkadmin
 {
+  use AuthenticatesUsers;
     /**
      * Handle an incoming request.
      *
@@ -18,6 +19,14 @@ class checkadmin
       if(!\Auth::check()){
         return redirect('/backend/login');
       }
+
+      if( \Auth::user()->is_super_admin <= 0 ){
+        $this->guard()->logout();
+        $request->session()->flush();
+        $request->session()->regenerate();
+        return redirect('/backend/login')->with("isadmin", "Уучлаарай та админ эрхтэй хэрэглэгч биш байна");
+      }
+
       return $next($request);
     }
 }
